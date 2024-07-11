7cd13f99cb1c4b47b21a57c4d796d977ed4a39a7
Lagat100 <weldonkiprop1@gmail.com>
2024-07-11 20:53:42 +0000

    fix 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,10 +1,14 @@
--- Creates a table with unique users.
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
---SELECT id, name FROM students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
+-- Drop the users table if it exists
 DROP TABLE IF EXISTS users;
+
+-- Create a table with unique users
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
+
+-- 3 first students in the Batch ID=3 because Batch 3 is the best!
+-- Uncomment the following line to execute the SELECT statement:
+-- SELECT id, name FROM students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
+
